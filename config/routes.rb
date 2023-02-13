373f597f5721214aecc1c376017523109adf97ce
Grayce Muthui <graycemuthui20@gmail.com>
2023-02-13 17:55:32 +0300

    Create the customers and products schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # home page
+  root 'customers#index'
+  get '/cutomers', to: 'customers#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
