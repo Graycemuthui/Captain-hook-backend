ca54278a42180454ded557f54b39c5ef89764b8c
Grayce Muthui <graycemuthui20@gmail.com>
2023-03-08 17:21:44 +0300

    Add validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,9 @@
 class Customer < ApplicationRecord
   has_many :orders, dependent: :destroy
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  resources :order_details
-  resources :customers
-  resources :orders
-  resources :products
-  resources :categories
-  resources :transactions
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # home page
-  root 'customers#index'
-  get '/cutomers', to: 'customers#index'
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :order_details
+      resources :customers
+      resources :orders
+      resources :products
+      resources :categories
+      resources :transactions
+      # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+      # home page
+      root 'customers#index'
+
+      # Defines the root path route ("/")
+      # root "articles#index"
+    end
+  end
 end
